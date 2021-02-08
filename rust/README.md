3bfa04e853fa328ffed3c46f6545f9acd1c6f8b7
rackping-admin <support@rackping.com>
2021-02-07 23:59:28 -0800

    initial commit

---
+++ rust/README.md
@@ -0,0 +1,75 @@
+# RackPing API 2.0 Client Sample Rust Programs
+
+Rust sample programs that you can copy-and-paste to automate common RackPing monitoring tasks from the command line or cron.
+
+Rust is a strictly-typed programming language, so these samples are more suitable for programmers who already have experience with Rust.
+
+## Getting Started
+
+* [Create a FREE RackPing monitoring account!](https://www.rackping.com/cgi-bin/signup.cgi) (Ctrl-click or Cmd-click link to signup in new tab)
+* Download or copy-and-paste a program, then configure set.sh with your RackPing login and API key.
+* For a linux cron job, download or copy-and-paste a RackPing Monitoring client program and manually set the RackPing account login information in the script according to the explanation in set.sh
+
+```
+# linux cron entry to run a script every 5 minutes
+*/5 * * * * /path/to/script parameters >>/tmp/logfile.log 2>&1
+```
+
+### Prerequisites
+
+Install the following dependencies:
+
+```
+rustc --version
+OpenSSL and headers
+jq (optional for the demo.sh test harness)
+```
+
+On Mac OS X, you may need to link rustc:
+```
+sudo ln -s /usr/local/bin/rustc /usr/bin/rustc
+```
+
+### Installing
+
+For setting the permissions on all files in this folder:
+
+```
+./make.sh
+```
+
+For a single program file:
+
+```
+chmod 755 filename
+```
+
+## Documentation
+
+See the [RackPing API web page](https://www.rackping.com/api.html) for:
+
+* RackPing 2.0 API PDF
+* errata (recent changes)
+* interactive Live API demo in your browser.
+
+## Running the tests
+
+After installing all the files in this folder, you can use demo.sh as a test harness to create a temporary check and contact using your RackPing account:
+
+```
+source ../set.sh
+time ./demo.sh
+```
+
+## Version
+
+1.0
+
+## License
+
+Copyright RackPing USA 2013-2021
+
+## Keywords
+
+RackPing REST API, SDK, Monitoring, Observability, Nocode, No-code, programming, scripting, automation.
+
---
+++ rust/rp_list_contacts.rs
@@ -0,0 +1,64 @@
+// Program: rp_list_contacts.rs
+// Usage: ./rp_list_contacts.rs
+// Date: 2021 02 06
+// Purpose: rust language sample client program for RackPing Monitoring API 2.0
+// Version: 1.0
+// Copyright: RackPing USA 2021
+// Env: rust (customize source code for your version of rust)
+// Returns: exit status is non-zero on failure
+// Notes:
+// - first do: source ../set.sh
+// - this alpha (non-functional) sample code is a template for advanced rust programmers to customize for their version of rust:
+//   - install rust and find out the version
+//   - decide if you want async or blocking call to get
+//   - update the main() definition with the correct signature if async
+//   - update the call to get
+//   - update the response println()
+//   - See:
+//     - https://github.com/seanmonstar/reqwest
+//     - https://github.com/seanmonstar/reqwest/blob/master/examples/blocking.rs
+//     - https://docs.rs/reqwest/0.9.20/reqwest/struct.Response.html
+
+use reqwest::blocking::Client;
+use std::env;
+use std::process;
+use std::time::Duration;
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+   let username   = env::var("RP_USER").unwrap();
+   let password   = env::var("RP_PASSWORD").unwrap();
+   let api_key    = env::var("RP_API_KEY").unwrap();
+   let user_agent = env::var("RP_USERAGENT").unwrap();
+  
+   if username == "" {
+      println!("error: do 'source ../set.sh' first.\n");
+      process::exit(1);
+   }
+
+   let domain     = env::var("RP_DOMAIN").unwrap();
+   let base_url   = env::var("RP_BASE_URL").unwrap();
+   let scheme     = env::var("RP_SCHEME").unwrap();
+
+   let timeout    = env::var("RP_TIMEOUT").unwrap().parse::<u64>().unwrap();
+   let _redirects = env::var("RP_REDIRECTS").unwrap().parse::<u32>().unwrap();
+   let _debug     = env::var("RP_DEBUG").unwrap().parse::<u8>().unwrap();
+
+   let url        = scheme.to_string() + &domain.to_string() + &base_url.to_string() + "/contacts";
+
+   println!("Get list of contacts\n");
+
+   let client = Client::new();
+   let resp = client.get(&url)
+       .header("User-agent", user_agent)
+       .header("app-key", api_key)
+       .basic_auth(username, Some(String::from(password)))
+       .timeout(Duration::from_secs(timeout))
+       .json::<HashMap<String, String>>();
+
+   println!("{:#?}", resp);
+   Ok(());
+
+   process::exit(0);
+}
+
