ea29a8049f9c5ce4432ec5796f3dc02d80a68aea
rackping-admin <support@rackping.com>
2021-02-11 18:52:29 -0800

    initial commit

--- rust/rp_list_contacts/src/rp_list_contacts.rs
+++ rust/rp_list_contacts/src/rp_list_contacts.rs
@@ -4,21 +4,16 @@
 // Purpose: rust language sample client program for RackPing Monitoring API 2.0
 // Version: 1.0
 // Copyright: RackPing USA 2021
-// Env: rust (customize source code for your version of rust)
+// Env: rust 1.49.0 (customize source code for your version of rust)
 // Returns: exit status is non-zero on failure
 // Notes:
 // - first do: source ../set.sh
-// - read doc/README.txt for programming info
+// - read doc/README.md for programming info
 
-use reqwest;
 use std::env;
 use std::process;
-use std::io::Read;
-// use serde_json;
-// use std::error::Error;
-// use std::result::Result;
-// use std::time::Duration;
-// use std::collections::HashMap;
+use std::time::Duration;
+use reqwest::blocking::Client;
 
 fn main() {
     let username   = env::var("RP_USER").unwrap_or("".to_string());
@@ -35,40 +30,62 @@ fn main() {
     let base_url   = env::var("RP_BASE_URL").unwrap();
     let scheme     = env::var("RP_SCHEME").unwrap();
 
-    let _timeout   = env::var("RP_TIMEOUT").unwrap().parse::<u64>().unwrap();
-    let _redirects = env::var("RP_REDIRECTS").unwrap().parse::<u32>().unwrap();
+    let timeout    = env::var("RP_TIMEOUT").unwrap().parse::<u64>().unwrap();
     let debug      = env::var("RP_DEBUG").unwrap().parse::<u8>().unwrap();
 
     let url        = scheme.to_string() + &domain.to_string() + &base_url.to_string() + "/contacts";
 
     eprintln!("info: get list of contacts\n");
 
-    let client  = reqwest::blocking::Client::new();
+let custom = reqwest::redirect::Policy::custom(|attempt| {
+    let redirects  = env::var("RP_REDIRECTS").unwrap().parse::<usize>().unwrap();
 
-//   let client = reqwest::blocking::Client::builder()
-//    .timeout(Duration::from_secs(10))
-//    .build()?;
+    if attempt.previous().len() > redirects {
+        attempt.error("too many redirects")
+    } else if attempt.url().host_str() == Some("example.domain") {
+        // prevent redirects to 'example.domain'
+        attempt.stop()
+    } else {
+        attempt.follow()
+    }
+});
 
-    let mut resp = client.get(&url)
-        .header("User-agent", user_agent)
-        .header("app-key", api_key)
-        .basic_auth(username, Some(String::from(password)))
-        .header("Accept","application/json")
-        .header("Accept-Charset","utf-8")
-//      .json::<HashMap<String, String>>();
-//      .json::<serde_json::Value>();
-//      .timeout(Duration::from_secs(_timeout))
-        .send()
-        .unwrap();
+    let client = Client::builder()
+       .timeout(Duration::from_secs(timeout))
+       .redirect(custom)
+       .build()
+       .unwrap();
 
-    let mut body = String::new();
-    resp.read_to_string(&mut body).unwrap();
+    let resp = client.get(&url)
+       .header("Accept","application/json")
+       .header("Accept-Charset","utf-8")
+       .header("User-agent", user_agent)
+       .header("app-key", api_key)
+       .basic_auth(username, Some(String::from(password)))
+       .send();
+
+    let resp = match resp {
+        Ok(resp) => resp,
+        Err(err) => {
+            println!("error: during networking: {}", err);
+            process::exit(1);
+        }
+    };
 
     if debug == 1 {
-       println!("Status: {}", resp.status());
-       println!("Headers:\n{:#?}", resp.headers());
+       eprintln!("status={}", resp.status());
+       eprintln!("headers={:#?}", resp.headers());
     }
 
-    println!("{}", body);
+    let resp_body = resp.text();
+    let _resp_body = match resp_body {
+        Ok(resp) => {
+          println!("{}", resp);
+        },
+        Err(err) => {
+            println!("error: while receiving respponse body: {}", err);
+            process::exit(1);
+        }
+    };
 }
 
